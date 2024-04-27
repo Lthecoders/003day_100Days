5d6f9a4a52ffde98823c9937c4006b2c7c956a0b
Nikhil Gaikwad <162660405+Lthecoders@users.noreply.github.com>
2024-04-27 12:49:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+# myName = input("What's your name? ")
+# myLunch = input("What are you having for lunch? ")
+# print(myName, "is going to be chowing down on", myLunch, "very soon!")
+
+# yourName = input("Name: ")
+# whatYear = input("What year is it?: ")
+# print(yourName, "thinks it is", whatYear)
+
+# print("=== Your Song Generator ===")
+# print("""You'll be asked a bunch of questions
+# then we'll make you up an amazing
+# song, totally copyright free 😭""")
+# print()
+# person = input("Name a person famous for something good: ")
+# thing = input("Name a thing they did: ")
+# place = input("Name a place you like: ")
+# rhyme = input("Give me a verb that rhymes with your person's name: ")
+# print()
+# print("There was a person called", person)
+# print("Who did something cool like", thing, "at the wonderful", place,
+#       "where you'll find me", rhyme)
+"""
+=== Your Song Generator ===
+You'll be asked a bunch of questions
+then we'll make you up an amazing
+song, totally copyright free 😭
+
+Name a person famous for something good: nikhil
+Name a thing they did: created remarkable achivements in programming 
+Name a place you like: Mumbai
+Give me a verb that rhymes with your person's name: eating
+
+There was a person called nikhil
+Who did something cool like created remarkable achivements in programming  at the 
+wonderful Mumbai where you'll find me eating
+"""
+"---------------- Day 3 challenge-------------------"
+
+# food = input("Name a Food > ")
+# plant = input("Name a type of plant > ")
+# cooking = input("Name a method of cooking > ")
+# describe = input("what word describes burned food? > ")
+# houseold = input("Name a DIY item > ")
+# print()
+# print("MENU")
+# print(cooking, food, "with", describe, plant, "on a bed", houseold)
+"""
+Name a Food > burger
+Name a type of plant > cactus
+Name a method of cooking > deep frie 
+what word describes burned food? > blac
+Name a DIY item > paper boat
+
+MENU
+deep frie burger with blac cactus on a bed paper boat
+"""
+
+# name = input("inter you name ")
+# gender = input("what is you gender ? ")
+# place = input("where are you from? ")
+
+# print()
+# print("hello My name is",
+#       name,
+#       "and my gender is",
+#       gender,
+#       "and i am from",
+#       place,
+#       end=".")
+
+
+number = input("Give me a number: ")
+group = input("Give me a collective noun for a group of things: ")
+thing = input("Give me the name of a weird or wacky thing: ")
+print("No I don't think that", number, "is a", group, "of", thing,". That's just odd.")
